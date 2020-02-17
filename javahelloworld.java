615f4e2100e2099ac5699c31279acf96e0030fe0
dhiaakermi <60490354+dhiaakermi@users.noreply.github.com>
2020-02-17 15:10:21 +0100

    Update javahelloworld.java

--- javahelloworld.java
+++ javahelloworld.java
@@ -5,6 +5,7 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
+	        System.out.println("hello this is my new version");
 		System.out.println("my name is dhia akermi");
 		System.out.println("my binome  is yassine soltani");
     } 
